b1d0acbcd815ce835e743cf638fbbb514ebabf54
Mr.Little Bo <160293352+HackerActivists@users.noreply.github.com>
2024-06-20 23:46:19 +0800

    game.c

--- game.c
+++ game.c
@@ -2,23 +2,27 @@
 
 int main()
 {
-
     int choose = 1;
-    Display_Menu();
+    Display_Menu(); // 显示菜单
     printf("Your choose is:");
-    scanf("%d",&choose);
-
+    scanf("%d", &choose);
     do
     {
-        switch(choose)
+        switch (choose)
         {
-            case 1:
-                Run_Game();   scanf("%d",&choose);  break;
-            default:
-                printf("Your choose is error , please afresh choose!!!\n");
-                printf("Your choose is:");
-                scanf("%d",&choose);        break;
-        }    
+        case 0: // 退出
+            printf("Exit succefssful!\n");
+            break;
+        case 1: // 游戏开始
+            Run_Game();
+            scanf("%d", &choose);
+            break;
+        default: // 输入错误，请重新输入
+            printf("Your choose is error , please afresh choose!!!\n");
+            printf("Your choose is:");
+            scanf("%d", &choose);
+            break;
+        }
     } while (choose);
 
     printf("----------------Games Over----------------\n");
